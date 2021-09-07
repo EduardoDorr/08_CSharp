e9df7e1e2222a8163eaf530a654e980bc7b72d4f
Eduardo da Silva Dörr <edudorr@hotmail.com>
2021-09-07 17:35:37 -0300

    20210907

--- ByteBank.AgencySystem/Program.cs
+++ ByteBank.AgencySystem/Program.cs
@@ -1,4 +1,5 @@
-﻿using ByteBank.Models;
+﻿using ByteBank.AgencySystem.Extensions;
+using ByteBank.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,17 @@ using System.Threading.Tasks;
 namespace ByteBank.AgencySystem {
   class Program {
     static void Main(string[] args) {
+      List<int> ages = new List<int>();
 
+      ages.Add(1);
+      ages.Add(15);
+      ages.Add(23);
+      ages.Add(35);
+      ages.AddMore(54, 25, 68, 32);
+
+      for (int i = 0; i < ages.Count; i++) {
+        Console.WriteLine(ages[i]);
+      }
 
       Console.WriteLine("The application finished! Key enter to exit!");
       Console.ReadLine();
